5269920a21d0d2366188372059e8b6e86b979aa7
manideep9243 <manideep8491@gmail.com>
2024-12-04 15:16:39 +0530

    first commit

---
+++ admin.js
@@ -0,0 +1,42 @@
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.getElementById("uploadButton").click();
+  }
+});
+
+document.getElementById("uploadButton").addEventListener("click", async () => {
+  const fileInput = document.getElementById("fileInput");
+  const formData = new FormData();
+
+  
+  // Check if a file is selected
+  if (!fileInput.files[0]) {
+    document.getElementById("uploadStatus").innerText = "Please select a file before uploading.";
+    document.getElementById("uploadStatus").style.color = "red";
+    return;
+  }
+
+  formData.append("file", fileInput.files[0]);
+
+  try {
+    const response = await fetch("https://adminbackend-axxb.onrender.com/admin/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const result = await response.json();
+    document.getElementById("uploadStatus").innerText = result.message;
+    document.getElementById("uploadStatus").style.color = "green";
+    console.log("Upload successful");
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    document.getElementById("uploadStatus").innerText = "Failed to upload file.";
+    document.getElementById("uploadStatus").style.color = "red";
+  }
+});
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.getElementById("uploadButton").click();
+  }
+});
